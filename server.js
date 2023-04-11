51582b6024fc9af1a421b35b48d2d7b0c6d68723
Julian <julian_sempre@hotmail.com>
2023-04-11 10:00:04 -0300

    initial commit

--- server.js
+++ server.js
@@ -1,49 +1,62 @@
-var express = require('express'),
-    path = require('path'),
-    nodeMailer = require('nodemailer'),
-    bodyParser = require('body-parser');
-	
-    var app = express();
-    app.set('view engine', 'ejs');
-    app.use(express.static('public'));
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(bodyParser.json());
-    var port = 3080;
-    app.get('/', function (req, res) {
-      res.render('http://localhost:3000/');
-    });
+const express = require('express');
+const path = require('path');
+const nodeMailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const app = express();
 
-    app.post('/send-email', function (req, res) {
-		let transporter = nodeMailer.createTransport({
-          host: 'smtp.gmail.com',
-          //port: 465,
-          secure: true,
-          auth: {
-              user: 'aerocopiasmidia@gmail.com',
-              pass: 'cgteyyravahgqwgx'
-          },
-		  tls:{
-        rejectUnauthorized: false
-      }
-      }); 
-      let mailOptions = {
-          from: '"Contato pelo site: " <aerocopiasmidia@gmail.com>', // sender address
-          to: "aerocopias@gmail.com", // list of receivers
-		  replyTo:'juliansilvacontato@gmail.com',
-          subject: req.body.nome + " entrou em contato pelo site", // Subject line
-          text:  "",// plain text body          
-          html: "Nome: " + req.body.nome + "<br> Email: " + req.body.senha + "<br> Mensagem: " + req.body.contato,
-      };
-	  
-      transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-              return console.log(error);
-          }
-          console.log('Message %s sent: %s', info.messageId, info.response);
-              res.render('index');
-          });
-      });
-          app.listen(port, function(){
-            console.log('Server is running at port: ',port);
-          });
+// Add proxy middleware
+app.use(
+  '/api',
+  createProxyMiddleware({
+    target: 'http://localhost:5000',
+    changeOrigin: true,
+  })
+);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+const port = 3080;
+
+app.get('/', function (req, res) {
+  res.render('http://localhost:3000/');
+});
+
+app.post('/send-email', function (req, res) {
+  const transporter = nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    //port: 465,
+    secure: true,
+    auth: {
+      user: 'aerocopiasmidia@gmail.com',
+      pass: 'cgteyyravahgqwgx',
+    },
+    tls:{
+      rejectUnauthorized: false,
+    },
+  });
+  const mailOptions = {
+    from: '"Contato pelo site: " <aerocopiasmidia@gmail.com>', // sender address
+    to: 'aerocopias@gmail.com', // list of receivers
+    replyTo: 'juliansilvacontato@gmail.com',
+    subject: req.body.nome + ' entrou em contato pelo site', // Subject line
+    text: '', // plain text body
+    html:
+      'Nome: ' + req.body.nome + '<br> Email: ' + req.body.senha + '<br> Mensagem: ' + req.body.contato,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+    res.redirect('http://localhost:3000/');
+  });
+});
+
+app.listen(port, function () {
+  console.log('Server is running at port: ', port);
+});
